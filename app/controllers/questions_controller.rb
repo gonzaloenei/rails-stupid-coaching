2b87c3d5b0ecbce60c47ef049c76eea3f651a035
Gonzalo Enei <gonzalo@fintual.com>
2021-02-15 14:49:59 -0300

    Solves 02

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @message = params[:message]
+    @answer =
+    if @message == "I am going to work"
+      "Great!"
+    elsif @message.ends_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
